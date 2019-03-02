e82a2db3b89514844795d24420098082ec62454b
gaby1994 <gabriel.degez@free.fr>
2019-03-02 18:33:43 +0100

    Add files via upload

---
+++ TestCarte.java
@@ -0,0 +1,11 @@
+public class TestCarte{
+    public static void main(String[] args){
+	Carte c1=new Carte();
+	System.out.println(c1);
+	/*Carte c2=new Carte("Energie");
+	System.out.println(c2);
+        Carte c3=new Carte("Dresseur");
+	System.out.println(c3);*/
+	
+    }
+}
